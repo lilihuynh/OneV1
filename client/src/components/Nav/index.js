3661bfdb97db47bc78746adf958f700533df8b31
Kai Warren <kaiwarren12@yahoo.com>
2021-02-14 19:00:49 -0800

    added logo image for navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from './One_v1.png'
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark" style={backgroundColor: ""}>
       <a className="navbar-brand" href="/">
+      <img src = {logo} alt="Logo"/>;
         React Reading List
       </a>
     </nav>
