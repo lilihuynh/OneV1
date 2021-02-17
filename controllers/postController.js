6e368d05c15e10d5627901ee09679a4acb5cedb0
Ion Gavrilita <gavrilita.ion.93@gmail.com>
2021-02-16 21:03:01 -0800

    changes commited

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,7 +25,6 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-
     remove: function (req, res) {
         db.Posts
             .findById({ _id: req.params.id })
