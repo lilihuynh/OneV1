abf350c6b7c328de6e1a7f520e0a0d269fbb3352
Kristine-1977 <kristinehrtn@yahoo.com>
2021-02-25 22:21:30 -0700

    restructuring files

--- single.js
+++
@@ -1,5 +0,0 @@
-app.post('/image-upload-single', (req, res) => {
-    const path = Object.values(Object.values(req.files)[0])[0].path
-    cloudinary.uploader.upload(path)
-        .then(image => res.json([image]))
-})
